a5be55de8a1d4aff3652ab146f7df38f29d441d1
tsrkzy <tsrmix@gmail.com>
2021-10-03 03:52:37 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import {
+    Router,
+    Link,
+    Route
+  } from "svelte-routing";
+  import Top from "./pages/Top.svelte";
+  import Gear from "./pages/Gear.svelte";
+  // import Test from "./pages/Test.svelte";
+
+</script>
+
+<Router>
+  <nav>
+    <Link to="">index</Link>
+    <Link to="gear">gear</Link>
+    <Link to="test">test</Link>
+  </nav>
+  <main>
+    <Route path="" component="{Top}"></Route>
+    <Route path="gear" component="{Gear}"></Route>
+<!--    <Route path="test" component="{Test}"></Route>-->
+  </main>
+</Router>
+
+<style>
+</style>
---
+++ src/pages/Top.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<main>
+  <h1>Top</h1>
+</main>
+
+<style>
+</style>
